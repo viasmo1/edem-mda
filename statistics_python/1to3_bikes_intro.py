d36eab89c64b8890551cdc09b293c9dfa584176e
Vicent Asensio <vicent.asenmol@gmail.com>
2020-10-09 12:08:19 +0200

    Printing directories

--- statistics_python/1to3_bikes_intro.py
+++ statistics_python/1to3_bikes_intro.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct  3 10:14:42 2020
@@ -17,17 +16,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Get working directory
-os.getcwd()
+print(os.getcwd())
 # Change working directory
-os.chdir('/Users/vicent/repos-github/mda/statistics_python/data/data_1to3')
-os.getcwd()
+os.chdir("/Users/vicent/repos-github/mda/statistics_python/data/data_1to3")
+print(os.getcwd())
 
 # %%
 # Read csv file and stores it in a dataframe called rentals_2011
-rentals_2011 = pd.read_csv('washington_bike_rentals_2011.csv', sep=';', decimal=',')
-rentals_2011.shape
-rentals_2011.head()
-rentals_2011.tail()
+rentals_2011 = pd.read_csv("washington_bike_rentals_2011.csv", sep=";", decimal=",")
+print(rentals_2011.shape)
+print(rentals_2011.tail())
+print(rentals_2011.head())
 # QC OK
 
 
@@ -37,30 +36,30 @@ rentals_2011.tail()
 x = rentals_2011.cnt
 
 # Plot
-plt.hist(x, edgecolor='black')
+plt.hist(x, edgecolor="black")
 plt.xticks(np.arange(0, 7000, step=1000))
-plt.title('Figure 1. Registered rentals in Wachington')
+plt.title("Figure 1. Registered rentals in Wachington")
 plt.show()
 
 
 # %%
 # Read weather info from weather_washington_2011.csv
-weather_2011 = pd.read_csv('weather_washington_2011.csv', sep=';', decimal=',')
-weather_2011.shape
-weather_2011.head()
-weather_2011.tail()
+weather_2011 = pd.read_csv("weather_washington_2011.csv", sep=";", decimal=",")
+print(weather_2011.shape)
+print(weather_2011.head())
+print(weather_2011.tail())
 # QC OK
 
 
 # %%
 # Merge rentals_2011 & weather_2011
-rentals_weather_2011 = pd.merge(weather_2011, rentals_2011, on='day')
+rentals_weather_2011 = pd.merge(weather_2011, rentals_2011, on="day")
 rentals_weather_2011.shape
 rentals_weather_2011.head()
 
 # Date is duplicated! Remove second version and rename the other one
-rentals_weather_2011 = rentals_weather_2011.drop(columns=['dteday_y'])
-rentals_weather_2011 = rentals_weather_2011.rename(columns={'dteday_x': 'dteday'})
+rentals_weather_2011 = rentals_weather_2011.drop(columns=["dteday_y"])
+rentals_weather_2011 = rentals_weather_2011.rename(columns={"dteday_x": "dteday"})
 rentals_weather_2011.shape
 rentals_weather_2011.head()
 
@@ -68,6 +67,6 @@ rentals_weather_2011.head()
 # %%
 # Scatter plot to analyse relationship between temp and rentals
 scatter = plt.scatter(x=rentals_weather_2011.temp_celsius, y=rentals_weather_2011.cnt)
-scatter.xlabel('Temperature')
-scatter.ylabel('# rentals')
+scatter.xlabel("Temperature")
+scatter.ylabel("# rentals")
 scatter.show()
