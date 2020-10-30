908d19c93e787d03a55db98d895ac3df9ae1c462
Vicent Asensio <vicent.asenmol@gmail.com>
2020-10-30 15:10:12 +0100

    Update file

--- 1_fundamentals/statistics_python/PEP7_mean_comparison_preparation.py
+++ 1_fundamentals/statistics_python/PEP7_mean_comparison_preparation.py
@@ -5,51 +5,56 @@ Created on Thu Oct 17 17:08:04 2019
 Mean Comparison
 MDA EDEM
 """
-#Resets ALL (Careful This is a "magic" function then it doesn't run as script) 
-#reset -f   
+# Resets ALL (Careful This is a "magic" function then it doesn't run as script)
+# reset -f
 
-#load basiclibraries
+# load basiclibraries
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 # New libraries
-from pandas.api.types import CategoricalDtype #For definition of custom categorical data types (ordinal if necesary)
-import scipy.stats as stats  # For statistical inference 
+from pandas.api.types import (
+    CategoricalDtype,
+)  # For definition of custom categorical data types (ordinal if necesary)
+import scipy.stats as stats  # For statistical inference
 import seaborn as sns  # For hi level, Pandas oriented, graphics
 
 # Get working directory
 os.getcwd()
 
 # Change working directory
-os.chdir('/Users/vicent/repos-github/edem-mda/1_fundamentals/statistics_python/data')
+os.chdir(
+    "/Users/vicent/repos-github/edem-mda/1_fundamentals/statistics_python/data/lessons"
+)
 os.getcwd()
 
-#Reads data from CSV file and stores it in a dataframe called rentals_2011
+# Reads data from CSV file and stores it in a dataframe called rentals_2011
 # Pay atention to the specific format of your CSV data (; , or , .)
-wbr = pd.read_csv ("WBR_11_12_denormalized_temp.csv", sep=';', decimal=',')
+wbr = pd.read_csv("WBR_11_12_denormalized_temp.csv", sep=";", decimal=",")
 print(wbr.shape)
 print(wbr.head())
 print(wbr.info())
-#QC OK
+# QC OK
 
 # Recode  working day
 # To string
 wbr["wd_st"] = wbr.workingday
 wbr.wd_st = wbr.wd_st.replace(to_replace=0, value="No")
 wbr.wd_st = wbr.wd_st.replace(to_replace=1, value="Yes")
-#To category
-my_categories=["No", "Yes"]
+# To category
+my_categories = ["No", "Yes"]
 my_datatype = CategoricalDtype(categories=my_categories, ordered=True)
 wbr["wd_cat"] = wbr.wd_st.astype(my_datatype)
 wbr.info()
 
-#frequencies
-mytable = pd.crosstab(index=wbr["wd_cat"], columns="count") # Crosstab
-n=mytable.sum()
-mytable2 = (mytable/n)*100
+# frequencies
+mytable = pd.crosstab(index=wbr["wd_cat"], columns="count")  # Crosstab
+n = mytable.sum()
+mytable2 = (mytable / n) * 100
 print(mytable2)
-plt.bar(mytable2.index, mytable2['count'])
-plt.xlabel('Working Day')
-plt.title('Figure 5. Percentage of Working Days')
+plt.bar(mytable2.index, mytable2["count"])
+plt.xlabel("Working Day")
+plt.title("Figure 5. Percentage of Working Days")
+
